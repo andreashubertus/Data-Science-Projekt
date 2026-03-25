3e581b96e1e74ac13a9358443703bf4db6b046c0
Vitalii <105307395+Sulphur1407@users.noreply.github.com>
2026-03-25 22:35:36 +0100

    add a interface for send_latest_newsletter

--- mailing/newsletter_sender.py
+++ mailing/newsletter_sender.py
@@ -1,11 +1,13 @@
-"""
-Docstring for mailing.newsletter_sender
+from models import DeliveryResult
 
-1. get summary from DB
-2. get subscribers from DB
-3. convert DB data → dataclasses (mappers)
-4. build email content
-5. send to each subscriber
-6. collect results
-7. save results in DB
-"""
+def send_latest_newsletter(db_handler) -> list[DeliveryResult]:
+    """
+    1. get summary from DB
+    2. get subscribers from DB
+    3. convert DB data → dataclasses (mappers)
+    4. build email content
+    5. send to each subscriber
+    6. collect results
+    7. save results in DB (call db_handler.save_delivery_result(...))
+    """
+    pass
