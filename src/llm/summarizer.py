993d852e045a4871b10764297713543ac5017df2
SebastianRein518 <sebastian-rein.8@web.de>
2026-03-28 13:11:56 +0100

    feat: summarize article function

--- src/llm/summarizer.py
+++ src/llm/summarizer.py
@@ -5,3 +5,17 @@ from groq import Groq
 load_dotenv()
 
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+
+def summarize_article(article: str) -> str:
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "system",
+                "content": "Du bist ein hilfreicher Assistent der Nachrichtenartikel präzise und neutral zusammenfasst."
+            },
+            {
+                "role": "user",
+                "content": ""
+            }])
+    return response.choices[0].message.content
