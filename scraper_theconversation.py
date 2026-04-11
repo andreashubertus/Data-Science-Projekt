6efa9023a21a586b2d277948e993d89da5404b65
andreashubertus <andreas2006hubertus@gmail.com>
2026-04-11 17:54:11 +0200

    refactor: move theconversation scraper to a separate file

---
+++ scraper_theconversation.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import time
+
+
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    
+
+
+def get_links_from_theconversation_rss():
+    rss_url = "https://theconversation.com/global/articles.atom"
+    request = requests.get(rss_url, headers=headers)
+    soup = BeautifulSoup(request.content, "xml")
+    links = [link['href'] for link in soup.find_all('link') if 'rel' in link.attrs and link['rel'] == 'alternate']
+    
+    return links
+
+
+def scrape_article(link):
+    return [link]
+
+
+def scrape_theconversation():
+    articles = []
+    articles_link = get_links_from_theconversation_rss()
+    if articles_link is None:
+        print("Keine Artikel gefunden.")
+        return []
+    else:
+        for link in articles_link:
+            articles.append(scrape_article(link))
+            time.sleep(2)
+    return articles
+
+
+if __name__ == "__main__":
+    theconversation_articles = scrape_theconversation()
+    print("the conversation abgeschlossen")
+    # for article in theconversation_articles:
+    #     print(f"Artikel-Link: {article[1]}")
